23f729007898f0a0bd98eac683ad3cad6f79fa0f
Aidan Beggs <nadiasggeb001@gmail.com>
2021-03-02 01:09:54 -0500

    Refactor flag checking logic to use bootstrap form verification.
    Flag checking now uses bootstrap form verification to display
    success/error message, though we still manually modify text/background
    color directly in the DOM.

--- assets/js/checkFlag.js
+++ assets/js/checkFlag.js
@@ -5,6 +5,7 @@
      * Storage object we use to persist solved challenge information across page loads.
      */
     const solvedStorage = window.localStorage;
+    solvedStorage.setItem("WARNING", "Don't even think about it!");
 
     /**
      * Key we use to access/set solved challenges in our storage object of choice.
@@ -12,30 +13,21 @@
     const solvedChallengesKey = "solvedChallenges";
 
     /**
-     * Checks to see if the chall with the given name is successfully solved, according to data currently entered on the page.
+     * Checks to see if the challenge with the given name is solved, according to data entered on the page.
      * 
-     * We check this by comparing the MD5 hash of the given flag against the true MD5. If correct, we store that this challenge
-     * has been solved, and display this information on the page.
+     * We check this by comparing the MD5 hash of the given flag against the true MD5. This MD5 value should be
+     * stored on the page under an element with the ID ${challengeName}-flag-md5.
      * 
      * @param {String} challName The name of the challenge to check.
      */
-    const checkFlag = (challName) => {
+    const isSolved = (challName) => {
         let challengeForm = document.getElementById(challName);
         let flagInput = document.getElementById(`${challName}-flag`);
         let flag = flagInput.value;
         let correctMd5 = document.getElementById(`${challName}-flag-md5`).innerText;
         let flagMd5 = hex_md5(flag);
         
-        if(flagMd5.toLowerCase() === correctMd5.toLowerCase()) {
-            challengeForm.classList.remove("challenge-incorrect");
-            challengeForm.classList.add("challenge-solved");
-            storeChallengeSolved(challName);
-            return true;
-        }
-
-        challengeForm.classList.remove("challenge-solved");
-        challengeForm.classList.add("challenge-incorrect");
-        return false;
+        return flagMd5.toLowerCase() == correctMd5.toLowerCase();
     }
 
     /**
@@ -44,7 +36,7 @@
      * 
      * @param {String} challName The name of the challenge to check.
      */
-    const hasChallengeBeenSolved = (challName) => {
+    const challengePreviouslySolved = (challName) => {
         // JSON.parse correctly handles the case where getItem returns null.
         let solvedChalls = JSON.parse(solvedStorage.getItem(solvedChallengesKey));
 
@@ -69,21 +61,62 @@
         solvedStorage.setItem(solvedChallengesKey, JSON.stringify(solvedChalls));
     }
 
+    /**
+     * Render the challenge with the given name as "solved" in the DOM.
+     * @param {String} challName The challenge name to set as solved.
+     */
+    const renderChallengeSolved = (challName) => {
+        let flagInput = document.getElementById(`${challName}-flag`);
 
+        flagInput.classList.remove("is-invalid");
+        flagInput.classList.add("is-valid");
+        flagInput.style.backgroundColor = "lightgreen";
+
+        let challengeTitle = document.getElementById(`${challName}-title`);
+
+        challengeTitle.style.color = "green";
+    }
+
+    /**
+     * Render the challenge with the given name as "failed" in the DOM.
+     * @param {String} challName The challenge name to set as failed.
+     */
+    const renderChallengeFailed = (challName) => {
+        let flagInput = document.getElementById(`${challName}-flag`);
+
+        flagInput.classList.remove("is-valid");
+        flagInput.classList.add("is-invalid");
+        
+        flagInput.style.backgroundColor = "";
+
+        let challengeTitle = document.getElementById(`${challName}-title`);
+
+        challengeTitle.style.color = "";
+    }
+
+
+    //////////////////////// ONLOAD LOGIC //////////////////////////////////
     let challengeForms = document.querySelectorAll(".challenge");
 
     challengeForms.forEach((challengeForm) => {
         let challengeName = challengeForm.id;
 
         // If this challenge form has been previously solved, add the appropriate CSS classes.
-        if(hasChallengeBeenSolved(challengeName)) {
-            challengeForm.classList.add("challenge-solved");
+        if(challengePreviouslySolved(challengeName)) {
+            renderChallengeSolved(challengeName);
         }
 
         // For each challenge form, check for flag validity on submit and prevent default form submit.
         challengeForm.addEventListener("submit", (e) => {
-            checkFlag(challengeName);
+            if(isSolved(challengeName)) {
+                renderChallengeSolved(challengeName);
+                storeChallengeSolved(challengeName);
+            } else {
+                renderChallengeFailed(challengeName);
+            }
             e.preventDefault();
         });
     });
+
+    ///////////////// END ONLOAD LOGIC /////////////////////////////////////
 })();
